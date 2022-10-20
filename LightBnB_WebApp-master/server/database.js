da832dfda23554045ee35a9916b5cd77f2bb98ca
Jaxson-Baerg <jaxson.baerg@gmail.com>
2022-10-20 15:26:02 +0000

    Refactor get user with id request to pull from the database instead of json data

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -30,7 +30,10 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  return pool
+    .query(`SELECT * FROM users WHERE id=$1`, [id])
+    .then(result => {return Promise.resolve(result.rows)})
+    .catch(err => {console.log(err.message)});
 }
 exports.getUserWithId = getUserWithId;
 
